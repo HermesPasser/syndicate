5f4d5da265c8fe17a20c167046532ddf71fba20e
Hermes Passer <dsldouglas@hotmail.com>
2023-04-02 11:54:19 -0300

    Format repo with black

--- ui/system_tray.py
+++ ui/system_tray.py
@@ -3,35 +3,36 @@ import os
 from PyQt5.QtWidgets import QMenu, QSystemTrayIcon, QAction
 from PyQt5.QtGui import QIcon
 
-class SystemTray():
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
 
-		self.icon = QIcon(os.path.join("ui","res", "plus.png"))
-		self._tray = QSystemTrayIcon()
-		self._tray.setIcon(self.icon)
-		self._tray.setToolTip('Syndicate')
-		self._tray.activated.connect(self._fire_open)
+class SystemTray:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-		menu = QMenu()
-		self.open_action = menu.addAction('open')
-		self.exit_action = menu.addAction('exit')
-		self._tray.setContextMenu(menu)
-	
-	def _fire_open(self, e):
-		"""On system tray icon double click"""
-		if QSystemTrayIcon.DoubleClick == e:
-			self.open_action.activate(QAction.Trigger)
-	
-	def show_message(self, title, message):
-		was_visible = self._tray.isVisible
-		self._tray.show()
-		self._tray.showMessage(title, message, self.icon)
-		if (was_visible):
-			self._tray.hide()
+        self.icon = QIcon("ui/res/plus.png")
+        self._tray = QSystemTrayIcon()
+        self._tray.setIcon(self.icon)
+        self._tray.setToolTip("Syndicate")
+        self._tray.activated.connect(self._fire_open)
 
-	def show(self):
-		self._tray.show()
+        menu = QMenu()
+        self.open_action = menu.addAction("open")
+        self.exit_action = menu.addAction("exit")
+        self._tray.setContextMenu(menu)
 
-	def hide(self):
-		self._tray.hide()
+    def _fire_open(self, e):
+        """On system tray icon double click"""
+        if QSystemTrayIcon.DoubleClick == e:
+            self.open_action.activate(QAction.Trigger)
+
+    def show_message(self, title, message):
+        was_visible = self._tray.isVisible
+        self._tray.show()
+        self._tray.showMessage(title, message, self.icon)
+        if was_visible:
+            self._tray.hide()
+
+    def show(self):
+        self._tray.show()
+
+    def hide(self):
+        self._tray.hide()
