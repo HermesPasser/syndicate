a353b9337b0955d409ddc37488030275d992a411
Hermes Passer <dsldouglas@hotmail.com>
2023-04-02 11:54:19 -0300

    Format repo with black

--- syndicate.py
+++ syndicate.py
@@ -10,11 +10,14 @@ import uuid
 import re
 import dateutil
 
+
 def uuid_from_url(url):
-	return str(uuid.uuid3(uuid.NAMESPACE_URL, url))
+    return str(uuid.uuid3(uuid.NAMESPACE_URL, url))
+
 
 class FeedError(Exception):
-	pass
+    pass
+
 
 """
 	channels.json format:
@@ -30,66 +33,72 @@ class FeedError(Exception):
 }
 """
 
-DB_FOLDER = Path.home().joinpath('syndicate')
-DB_FILE = DB_FOLDER.joinpath('syndicate.db')
+DB_FOLDER = Path.home().joinpath("syndicate")
+DB_FILE = DB_FOLDER.joinpath("syndicate.db")
 DB_FOLDER.mkdir(exist_ok=True)
+
 
 # FIXME: folder need to be created
 class ChannelList:
-	def __init__(self, db_file_name=DB_FILE):
-		self.db_file = db_file_name
-		self.conn = sqlite3.connect(self.db_file)
-		self.cursor = self.conn.cursor()
-		self.channel_table_name = 'channel'
-		self.item_table_name = 'item'
-		self._callback = None
+    def __init__(self, db_file_name=DB_FILE):
+        self.db_file = db_file_name
+        self.conn = sqlite3.connect(self.db_file)
+        self.cursor = self.conn.cursor()
+        self.channel_table_name = "channel"
+        self.item_table_name = "item"
+        self._callback = None
 
-	@property
-	def channel_links(self):
-		self.cursor.execute(f"SELECT link from {self.channel_table_name}")
-		rows = self.cursor.fetchall()
-		rows = [row[0] for row in rows]
-		return rows
+    @property
+    def channel_links(self):
+        self.cursor.execute(f"SELECT link from {self.channel_table_name}")
+        rows = self.cursor.fetchall()
+        rows = [row[0] for row in rows]
+        return rows
 
-	@property
-	def channel_id_and_title(self):
-		self.cursor.execute(f"SELECT id, name from {self.channel_table_name}")
-		rows = self.cursor.fetchall()
-		rows = [(row[0], row[1]) for row in rows]
-		return rows
+    @property
+    def channel_id_and_title(self):
+        self.cursor.execute(f"SELECT id, name from {self.channel_table_name}")
+        rows = self.cursor.fetchall()
+        rows = [(row[0], row[1]) for row in rows]
+        return rows
 
-	def get_feed(self, channel_id):
-		self.cursor.execute(f"SELECT * FROM {self.item_table_name} WHERE channel = ?", (channel_id,))
-		rows = self.cursor.fetchall()
+    def get_feed(self, channel_id):
+        self.cursor.execute(
+            f"SELECT * FROM {self.item_table_name} WHERE channel = ?", (channel_id,)
+        )
+        rows = self.cursor.fetchall()
 
-		feed = {}
-		for row in rows:
-			id = row[0]
-			feed[id] =  {
-				'title': row[1],
-				'link': row[3],
-				'id': id,
-				'date': row[4],
-				'read': bool(row[5]),
-				'content': row[2],
-				'channel': channel_id
-			}
+        feed = {}
+        for row in rows:
+            id = row[0]
+            feed[id] = {
+                "title": row[1],
+                "link": row[3],
+                "id": id,
+                "date": row[4],
+                "read": bool(row[5]),
+                "content": row[2],
+                "channel": channel_id,
+            }
 
-		return feed
-	
-	def open(self):
-		self._create_db()
+        return feed
 
-	def _create_db(self):
-		self.cursor.execute(f"""
+    def open(self):
+        self._create_db()
+
+    def _create_db(self):
+        self.cursor.execute(
+            f"""
 			CREATE TABLE IF NOT EXISTS {self.channel_table_name} (
 				id 		VARCHAR(36) NOT NULL PRIMARY KEY,
 				name	VARCHAR(30) NOT NULL,
 				link  	VARCHAR(120)
 			);
-		""")
+		"""
+        )
 
-		self.cursor.execute(f"""
+        self.cursor.execute(
+            f"""
 			CREATE TABLE IF NOT EXISTS {self.item_table_name} (
 				id 		VARCHAR(36) NOT NULL PRIMARY KEY,
 				title 	VARCHAR(160) NOT NULL,
@@ -100,125 +109,142 @@ class ChannelList:
 				channel	VARCHAR(36) NOT NULL,
 				FOREIGN KEY(channel) REFERENCES {self.channel_table_name}(id)
 			);
-		""")
-		self.conn.commit()
-	
-	def subscribe(self, callback):
-		self._callback = callback
-	
-	def close(self):
-		self.cursor.close()
-		self.conn.commit()
-		self.conn.close()
+		"""
+        )
+        self.conn.commit()
 
-	def channel_exists(self, id):
-		self.cursor.execute(f"SELECT name FROM {self.channel_table_name} WHERE id=?", (id,))
-		exists = len(self.cursor.fetchall()) == 1
-		return exists
-	
-	def _feed_exists(self, id, channel_id):
-		self.cursor.execute(f"SELECT title FROM {self.item_table_name} WHERE id=? and channel=?", (id,channel_id))
-		exists = len(self.cursor.fetchall()) == 1
-		return exists
+    def subscribe(self, callback):
+        self._callback = callback
 
-	def add_channel(self, name, url):
-		ch_id = uuid_from_url(url)
-		if self.channel_exists(ch_id):
-			raise FeedError(f'"{name}" channel (of id: {ch_id}) already exists')
-		
-		self.cursor.execute(f"INSERT INTO {self.channel_table_name} (id, name, link) VALUES (?,?,?)", (ch_id, name, url))
-		self.conn.commit()
-		return ch_id
+    def close(self):
+        self.cursor.close()
+        self.conn.commit()
+        self.conn.close()
 
-	def add_feed_item(self, title, content, link, item_id, date, channel_id):
-		if  self._feed_exists(item_id, channel_id):
-			# ignore if already exists...
-			return
+    def channel_exists(self, id):
+        self.cursor.execute(
+            f"SELECT name FROM {self.channel_table_name} WHERE id=?", (id,)
+        )
+        exists = len(self.cursor.fetchall()) == 1
+        return exists
 
-		self.cursor.execute(
-			f"INSERT INTO {self.item_table_name} (id, title, content, link, date, channel) VALUES (?,?,?,?,?,?)", 
-			(item_id, title, content, link, date, channel_id)
-		)
-		self.conn.commit()
-		
-		item = {
-			'title': title,
-			'link': link,
-			'id': item_id,
-			'date': date,
-			'read': False,
-			'content': content,
-			'channel': channel_id
-		}
+    def _feed_exists(self, id, channel_id):
+        self.cursor.execute(
+            f"SELECT title FROM {self.item_table_name} WHERE id=? and channel=?",
+            (id, channel_id),
+        )
+        exists = len(self.cursor.fetchall()) == 1
+        return exists
 
-		# Notify of new items
-		# TODO: since this already is returning the item, maybe create a 
-		# new method just to check if is a new item and notify? But
-		# in this case we should add a func only to load the items
-		# that already are on the json.
-		# NOTE: since we don't call it on another thread, if the
-		# callback does something on the thread and never
-		# returns (like  funcs calling other funcs forever..)
-		# we may encounter some troubles
-		if self._callback is not None:
-			self._callback(item)
+    def add_channel(self, name, url):
+        ch_id = uuid_from_url(url)
+        if self.channel_exists(ch_id):
+            raise FeedError(f'"{name}" channel (of id: {ch_id}) already exists')
 
-		return item
+        self.cursor.execute(
+            f"INSERT INTO {self.channel_table_name} (id, name, link) VALUES (?,?,?)",
+            (ch_id, name, url),
+        )
+        self.conn.commit()
+        return ch_id
 
-	def mark_feed_item_as(self, channel_id, item_id, is_read):
-		if not self._feed_exists(item_id, channel_id):
-			raise FeedError(f"Could not set feed's reading status. The item with {item_id} id does not exists")
-		
-		self.cursor.execute(f"UPDATE {self.item_table_name} SET read=? WHERE id=?", (int(is_read), item_id))
-		self.conn.commit()
+    def add_feed_item(self, title, content, link, item_id, date, channel_id):
+        if self._feed_exists(item_id, channel_id):
+            # ignore if already exists...
+            return
+
+        self.cursor.execute(
+            f"INSERT INTO {self.item_table_name} (id, title, content, link, date, channel) VALUES (?,?,?,?,?,?)",
+            (item_id, title, content, link, date, channel_id),
+        )
+        self.conn.commit()
+
+        item = {
+            "title": title,
+            "link": link,
+            "id": item_id,
+            "date": date,
+            "read": False,
+            "content": content,
+            "channel": channel_id,
+        }
+
+        # Notify of new items
+        # TODO: since this already is returning the item, maybe create a
+        # new method just to check if is a new item and notify? But
+        # in this case we should add a func only to load the items
+        # that already are on the json.
+        # NOTE: since we don't call it on another thread, if the
+        # callback does something on the thread and never
+        # returns (like  funcs calling other funcs forever..)
+        # we may encounter some troubles
+        if self._callback is not None:
+            self._callback(item)
+
+        return item
+
+    def mark_feed_item_as(self, channel_id, item_id, is_read):
+        if not self._feed_exists(item_id, channel_id):
+            raise FeedError(
+                f"Could not set feed's reading status. The item with {item_id} id does not exists"
+            )
+
+        self.cursor.execute(
+            f"UPDATE {self.item_table_name} SET read=? WHERE id=?",
+            (int(is_read), item_id),
+        )
+        self.conn.commit()
+
 
 # FIXME: it seems it doesnt add a second channel to the feed
 # i think it does not anymore, check it out
 
+
 def mili_to_date(float):
-	return datetime.fromtimestamp(float)
+    return datetime.fromtimestamp(float)
 
 
-def str_date_to_mili(str_date : str) -> int:
-	"""Construct a POSIX timestamp from a date string."""
-	try:
-		return int(dateutil.parser.parse((str_date)).timestamp())
-	except ValueError as ex:
-		print('str_date_to_mili:', str_date, ex)
-		return 0
+def str_date_to_mili(str_date: str) -> int:
+    """Construct a POSIX timestamp from a date string."""
+    try:
+        return int(dateutil.parser.parse((str_date)).timestamp())
+    except ValueError as ex:
+        print("str_date_to_mili:", str_date, ex)
+        return 0
 
 
 # FIXME: erros with urls:
 # - forbidden: https://www.podcloud.com.br/feed/podcaps
 def fetch_rss(url):
-	# TODO: error handling when is not 200
-	raw_text = ''
-	with req.urlopen(url, cafile=certifi.where()) as response:
-		raw_text = response.read()
+    # TODO: error handling when is not 200
+    raw_text = ""
+    with req.urlopen(url, cafile=certifi.where()) as response:
+        raw_text = response.read()
 
-	return raw_text
+    return raw_text
 
 
+def parse_rss(feed, text, url, channel_name=""):
+    # this is the happy path, we need to handle malformated xml
 
-def parse_rss(feed, text, url, channel_name=''):
-	# this is the happy path, we need to handle malformated xml
+    root = ET.fromstring(text)
+    channel = root.find("channel")
+    if channel_name == "":
+        channel_name = channel.find("title").text
 
-	root = ET.fromstring(text)
-	channel = root.find('channel')
-	if channel_name == '':
-		channel_name = channel.find('title').text
+    ch_id = uuid_from_url(url)
+    if not feed.channel_exists(ch_id):
+        ch_id = feed.add_channel(
+            channel_name, url
+        )  # feed's url, not the embeded link inside of it
 
-	ch_id = uuid_from_url(url)
-	if not feed.channel_exists(ch_id):
-		ch_id = feed.add_channel(channel_name, url) # feed's url, not the embeded link inside of it
-	
-	for item in channel.findall('item'):
-		feed.add_feed_item(
-			# TODO: can't figure how get <content:encoded> tag with etree, so we're going with description instead
-			item.find('title').text,
-			item.find('description').text,
-			item.find('link').text,
-			item.find('guid').text,
-			str_date_to_mili(item.find('pubDate').text),
-			ch_id
-		)
+    for item in channel.findall("item"):
+        feed.add_feed_item(
+            # TODO: can't figure how get <content:encoded> tag with etree, so we're going with description instead
+            item.find("title").text,
+            item.find("description").text,
+            item.find("link").text,
+            item.find("guid").text,
+            str_date_to_mili(item.find("pubDate").text),
+            ch_id,
+        )
--- tests.py
+++ tests.py
@@ -1,100 +1,106 @@
-from syndicate import ChannelList, uuid_from_url
 import unittest
+from syndicate import ChannelList, uuid_from_url
+from rss.tests.test_parser import *
+
 
 class ChannelListTest(unittest.TestCase):
-	feed: ChannelList
+    feed: ChannelList
 
-	def setUp(self):
-		self.feed = ChannelList(':memory:')
-		self.feed.open()
+    def setUp(self):
+        self.feed = ChannelList(":memory:")
+        self.feed.open()
 
-	def tearDown(self):
-		self.feed.close()
-	
-	def test_add_channel(self):
-		test_url = 'test url'
-		self.feed.add_channel('test channel', test_url)
+    def tearDown(self):
+        self.feed.close()
 
-		ch_id = uuid_from_url(test_url)
-		exists = self.feed.channel_exists(ch_id)
+    def test_add_channel(self):
+        test_url = "test url"
+        self.feed.add_channel("test channel", test_url)
 
-		self.assertTrue(exists)
+    def test_add_channel(self):
+        test_url = "test url"
+        self.feed.add_channel("test channel", test_url)
 
-	def test_add_channel_not_exists(self):
-		self.feed.add_channel('test channel', 'test url')
+        ch_id = uuid_from_url(test_url)
+        exists = self.feed.channel_exists(ch_id)
 
-		ch_id = uuid_from_url('anothertest url')
-		exists = self.feed.channel_exists(ch_id)
+        self.assertTrue(exists)
 
-		self.assertFalse(exists)
+    def test_add_channel_not_exists(self):
+        self.feed.add_channel("test channel", "test url")
 
-	def test_add_item(self):
-		test_url = 'test url'
-		self.feed.add_channel('test channel', test_url)
+        ch_id = uuid_from_url("anothertest url")
+        exists = self.feed.channel_exists(ch_id)
 
-		ch_id = uuid_from_url(test_url)
-		contents = 'content1'
-		title = 'item1'
-		link = 'link'
-		date = 55555
-		uid = 11
+        self.assertFalse(exists)
 
-		item = self.feed.add_feed_item(title, contents, link, uid, date, ch_id)
+    def test_add_item(self):
+        test_url = "test url"
+        self.feed.add_channel("test channel", test_url)
 
-		self.assertEqual(item['content'], contents)
-		self.assertEqual(item['channel'], ch_id)
-		self.assertEqual(item['title'], title)
-		self.assertEqual(item['read'], False)
-		self.assertEqual(item['link'], link)
-		self.assertEqual(item['date'], date)
-		self.assertEqual(item['id'], uid)
+        ch_id = uuid_from_url(test_url)
+        contents = "content1"
+        title = "item1"
+        link = "link"
+        date = 55555
+        uid = 11
 
-	def test_mark_item_as_read(self):
-		test_url = 'test url'
-		ch_id = uuid_from_url(test_url)
-		uid = '11'
+        item = self.feed.add_feed_item(title, contents, link, uid, date, ch_id)
 
-		self.feed.add_channel('test channel', test_url)
-		self.feed.add_feed_item('item1', 'content1', 'link', uid, 55555, ch_id)
-		self.feed.mark_feed_item_as(ch_id, uid, True)
+        self.assertEqual(item["content"], contents)
+        self.assertEqual(item["channel"], ch_id)
+        self.assertEqual(item["title"], title)
+        self.assertEqual(item["read"], False)
+        self.assertEqual(item["link"], link)
+        self.assertEqual(item["date"], date)
+        self.assertEqual(item["id"], uid)
 
-		items = self.feed.get_feed(ch_id)
-		self.assertEqual(items[uid]['read'], True)
+    def test_mark_item_as_read(self):
+        test_url = "test url"
+        ch_id = uuid_from_url(test_url)
+        uid = "11"
 
-	def test_subscribe(self):
-		test_url = 'test url'
-		ch_id = uuid_from_url(test_url)
-		item_name = 'item1'
+        self.feed.add_channel("test channel", test_url)
+        self.feed.add_feed_item("item1", "content1", "link", uid, 55555, ch_id)
+        self.feed.mark_feed_item_as(ch_id, uid, True)
 
-		self.feed.add_channel('test channel', test_url)
+        items = self.feed.get_feed(ch_id)
+        self.assertEqual(items[uid]["read"], True)
 
-		self.feed.subscribe(lambda item: self.assertEqual(item_name, item['title']))
-		self.feed.add_feed_item(item_name, 'content1', 'link', 1, 55555, ch_id)
+    def test_subscribe(self):
+        test_url = "test url"
+        ch_id = uuid_from_url(test_url)
+        item_name = "item1"
 
-	def test_multiple_channels_with_items(self):
-		ch1_url = 'test url'
-		ch1_id = uuid_from_url(ch1_url)
-		ch1_item1_id = '1'
-		self.feed.add_channel('test channel', ch1_url)
-		self.feed.add_feed_item('ch1 item1', '', '', ch1_item1_id, 0, ch1_id)
+        self.feed.add_channel("test channel", test_url)
 
-		ch2_url = 'test url2'
-		ch2_id = uuid_from_url(ch2_url)
-		ch2_item1_id = '3'
-		self.feed.add_channel('test channel', ch2_url)
-		self.feed.add_feed_item('ch1 item1', '', '', ch2_item1_id, 0, ch2_id)
-		
-		ch1_item2_id = '2'
-		self.feed.add_feed_item('ch1 item2', '', '', ch1_item2_id, 0, ch1_id)
+        self.feed.subscribe(lambda item: self.assertEqual(item_name, item["title"]))
+        self.feed.add_feed_item(item_name, "content1", "link", 1, 55555, ch_id)
 
-		ch1_items = self.feed.get_feed(ch1_id)
-		ch2_items = self.feed.get_feed(ch2_id)
+    def test_multiple_channels_with_items(self):
+        ch1_url = "test url"
+        ch1_id = uuid_from_url(ch1_url)
+        ch1_item1_id = "1"
+        self.feed.add_channel("test channel", ch1_url)
+        self.feed.add_feed_item("ch1 item1", "", "", ch1_item1_id, 0, ch1_id)
 
-		self.assertEqual(len(ch1_items.keys()), 2)
-		self.assertEqual(ch1_items[ch1_item1_id]['id'], ch1_item1_id)
-		self.assertEqual(ch1_items[ch1_item2_id]['id'], ch1_item2_id)
-		self.assertEqual(ch2_items[ch2_item1_id]['id'], ch2_item1_id)
-		
+        ch2_url = "test url2"
+        ch2_id = uuid_from_url(ch2_url)
+        ch2_item1_id = "3"
+        self.feed.add_channel("test channel", ch2_url)
+        self.feed.add_feed_item("ch1 item1", "", "", ch2_item1_id, 0, ch2_id)
 
-if __name__ == '__main__':
-	unittest.main()
+        ch1_item2_id = "2"
+        self.feed.add_feed_item("ch1 item2", "", "", ch1_item2_id, 0, ch1_id)
+
+        ch1_items = self.feed.get_feed(ch1_id)
+        ch2_items = self.feed.get_feed(ch2_id)
+
+        self.assertEqual(len(ch1_items.keys()), 2)
+        self.assertEqual(ch1_items[ch1_item1_id]["id"], ch1_item1_id)
+        self.assertEqual(ch1_items[ch1_item2_id]["id"], ch1_item2_id)
+        self.assertEqual(ch2_items[ch2_item1_id]["id"], ch2_item1_id)
+
+
+if __name__ == "__main__":
+    unittest.main()
